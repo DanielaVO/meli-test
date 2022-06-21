2a9afdf50c79ae199b01727df6a3e968b78a2d04
Daniela Villegas Osorio <dvillegasosorio@gmail.com>
2022-06-21 11:02:47 -0500

    Add api configuration

--- api/routes/items.js
+++ api/routes/items.js
@@ -1,5 +1,7 @@
 var express = require('express');
+require('dotenv').config()
 var router = express.Router();
+var fetch = require('fetch').fetchUrl;
 
 const author = {
   name: "Daniela",
@@ -7,57 +9,58 @@ const author = {
 };
 
 /* GET itens listing. */
-router.get('/', async function (req, res, next) {
+router.get('/', function (req, res, next) {
   let promise;
-  console.log('items');
   console.log('entre', `${process.env.API_ML_PRODUCTS_SEARCHER}?q=${req.query.q}`);
   console.log('req.query.q', req.query.q);
   if (req.query.q && req.query.q.trim()) {
-    promise = fetch(`${process.env.API_ML_PRODUCTS_SEARCHER}?q=${req.query.q}`).then(response => response.json());
+    promise = fetch(`${process.env.API_ML_PRODUCTS_SEARCHER}?q=${req.query.q}`).then(response => response.json())
+    console.log('promise', promise);
   } else {
     promise = Promise.resolve();
   }
-  const data = await promise;
-  console.log('data', data);
-  let categories = [];
-  let items = [];
-  if (data) {
-    categories = (data.filters || [])
-      .filter(category => category.id === "category")
-      .map(category_1 => category_1.values
-        .map(value_1 => value_1.path_from_root
-          .map(path => path.name)
-        )
-        .find(() => true)
-      )
-      .find(() => true);
+  return promise
+    .then(data => {
+      let categories = [];
+      let items = [];
+      if (data) {
+        categories = (data.filters || [])
+          .filter(category => category.id === "category")
+          .map(category_1 => category_1.values
+            .map(value_1 => value_1.path_from_root
+              .map(path => path.name)
+            )
+            .find(() => true)
+          )
+          .find(() => true);
 
-    let results = data.results || [];
-    for (let i = 0; i < 4 && i < results.length; i++) {
-      let article = results[i];
-      items.push({
-        id: article.id,
-        title: article.title,
-        price: {
-          currency: article.currency_id,
-          amount: article.available_quantity,
-          decimals: article.price
-        },
-        picture: article.thumbnail,
-        condition: article.condition,
-        free_shipping: (article.shipping && article.shipping.free_shipping === true),
-        location: {
-          state: article.address.state_name,
-          city: article.address.city_name
+        let results = data.results || [];
+        for (let i = 0; i < 4 && i < results.length; i++) {
+          let article = results[i];
+          items.push({
+            id: article.id,
+            title: article.title,
+            price: {
+              currency: article.currency_id,
+              amount: article.available_quantity,
+              decimals: article.price
+            },
+            picture: article.thumbnail,
+            condition: article.condition,
+            free_shipping: (article.shipping && article.shipping.free_shipping === true),
+            location: {
+              state: article.address.state_name,
+              city: article.address.city_name
+            }
+          });
         }
+      }
+      res.send({
+        author,
+        categories,
+        items
       });
-    }
-  }
-  res.send({
-    author,
-    categories,
-    items
-  });
+    });
 });
 
 module.exports = router;
